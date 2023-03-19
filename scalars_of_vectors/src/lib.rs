6885105b504d8aaa83fb87849d4e04da5d08df9d
Arthur Khakimov <verybigfolder@yandex.ru>
2023-03-20 00:23:29 +0500

    Reformat

--- scalars_of_vectors/src/lib.rs
+++ scalars_of_vectors/src/lib.rs
@@ -4,7 +4,7 @@ use rayon::prelude::*;
 /// use scalars_of_vectors::scalar;
 /// let a = vec![1, 2, 3];
 /// let b = vec![3, 4, 5];
-/// assert_eq!(scalar(&a,&b), 26);
+/// assert_eq!(scalar(&a, &b), 26);
 /// ```
 pub fn scalar(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
     v1.par_iter().zip(v2.par_iter()).map(|(a, b)| a * b).sum()
