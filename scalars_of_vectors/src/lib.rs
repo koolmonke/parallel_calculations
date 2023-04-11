4e032f963d451d8a5d37c482e1164b6142b4edbf
Arthur Khakimov <verybigfolder@yandex.ru>
2023-04-11 16:39:23 +0500

    Вынес тесты в отдельный модуль scalar

--- scalars_of_vectors/src/lib.rs
+++ scalars_of_vectors/src/lib.rs
@@ -2,15 +2,21 @@ use std::{iter::Sum, ops::Mul};
 
 use rayon::prelude::*;
 
-/// ```
-/// use scalars_of_vectors::scalar;
-/// let a = vec![1, 2, 3];
-/// let b = vec![3, 4, 5];
-/// assert_eq!(scalar(&a, &b), 26);
-/// ```
 pub fn scalar<'a, T: Send + Sync + Sum<T>>(v1: &'a Vec<T>, v2: &'a Vec<T>) -> T
 where
     &'a T: Mul<Output = T>,
 {
     v1.par_iter().zip(v2.par_iter()).map(|(a, b)| a * b).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scalar() {
+        let a = vec![1, 2, 3];
+        let b = vec![3, 4, 5];
+        assert_eq!(scalar(&a, &b), 26);
+    }
 }
