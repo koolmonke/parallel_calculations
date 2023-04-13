9dc5e64e968014b70e9af5e7f897f25c2623a811
Arthur Khakimov <verybigfolder@yandex.ru>
2023-04-13 18:57:08 +0500

    move to where

--- scalars_of_vectors/src/lib.rs
+++ scalars_of_vectors/src/lib.rs
@@ -2,9 +2,10 @@ use std::{iter::Sum, ops::Mul};
 
 use rayon::prelude::*;
 
-pub fn scalar<'a, T: Send + Sync + Sum<T>>(v1: &'a Vec<T>, v2: &'a Vec<T>) -> T
+pub fn scalar<'a, T>(v1: &'a Vec<T>, v2: &'a Vec<T>) -> T
 where
     &'a T: Mul<Output = T>,
+    T: Send + Sync + Sum<T>,
 {
     v1.par_iter().zip(v2.par_iter()).map(|(a, b)| a * b).sum()
 }
